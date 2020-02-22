4b768a2547e997886b0f8f44c187a433749c317c
Tom Chen <tom@itri.org.tw>
2020-02-22 12:25:53 +0800

    feat: async await practice

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { prisma } from '../generated/prisma-client';
+import { prisma, TweetPromise } from '../generated/prisma-client';
 
 const fName = faker.name.findName();
 const fZip = faker.address.zipCode();
@@ -8,27 +8,54 @@ const fTitle = faker.random.word();
 
 console.log(`faker zip: ${fZip}`);
 
-// create Tweet by User: 999
-prisma
-  .createTweet({
+// async awiat test:
+const createTweetforUser = async (authorId: string | number) => {
+  const creTweetsById = await creTweetById(authorId);
+  const targetUserwithTweets = await tweetsById(authorId);
+  console.log(
+    `target tweets by user id ${authorId} : ${JSON.stringify(targetUserwithTweets, undefined, 2)}`
+  );
+  return targetUserwithTweets;
+};
+
+const creTweetById = (uID: string | number): TweetPromise => {
+  return prisma.createTweet({
     title: fTitle,
     text: fText,
-    published: false,
-    owner: { connect: { id: 999 } }
-  })
-  .then(data => {
-    console.log(`create Tweet: ${JSON.stringify(data, undefined, 2)}`);
-    return prisma.tweets();
-  })
-  .then(data => {
-    console.log(
-      `after create tweet, query all tweets: ${JSON.stringify(
-        data,
-        undefined,
-        2
-      )}`
-    );
+    published: true,
+    owner: { connect: { id: uID } }
   });
+};
+
+const tweetsById = (uID: string | number) => {
+  const targetTest = prisma.tweets({ where: { owner: { id: uID } } });
+
+  return targetTest;
+};
+
+createTweetforUser('ck6vs1ch101z40891nhu9iflj');
+
+// create Tweet by User: 999
+// prisma
+//   .createTweet({
+//     title: fTitle,
+//     text: fText,
+//     published: false,
+//     owner: { connect: { id: 999 } }
+//   })
+//   .then(data => {
+//     console.log(`create Tweet: ${JSON.stringify(data, undefined, 2)}`);
+//     return prisma.tweets();
+//   })
+//   .then(data => {
+//     console.log(
+//       `after create tweet, query all tweets: ${JSON.stringify(
+//         data,
+//         undefined,
+//         2
+//       )}`
+//     );
+//   });
 
 // update user NAME and Zipcode by ID
 // prisma
